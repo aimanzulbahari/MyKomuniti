7ec161291e48026d12334d479fc243c73337ee73
aimanzulbahari-MBSA <aiman.zulbahari@mbsa.gov.my>
2025-11-13 17:37:38 +0800

    Organize the folder structure and add new file

---
+++ utama.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="/assets/img/MBSA-Emblem.svg" type="image/x-icon">
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="/assets/css/portal-css/portal.css">
+    <title>Portal MyMPP | MBSA</title>
+
+    <!-- Bootstrap CDN -->
+    <!-- Google Font CDN -->
+    <!-- SwiperJS -->
+</head>
+
+<body>
+
+    <!-- Script CDN -->
+    <!-- Bootstrap Script -->
+    <!-- SwiperJS Script -->
+</body>
+
+</html>
