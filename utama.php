dd4283d95748f0e6fd6f762cc0f0b2ecf3b2e0ea
aimanzulbahari-MBSA <aiman.zulbahari@mbsa.gov.my>
2025-11-14 14:13:15 +0800

    link cdn

--- utama.php
+++ utama.php
@@ -11,15 +11,84 @@
     <title>Portal Golden Parking | MBSA</title>
 
     <!-- Bootstrap CDN -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+
     <!-- Google Font CDN -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+
     <!-- SwiperJS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@12/swiper-bundle.min.css" />
 </head>
 
 <body>
 
+    <!-- Navbar - Start -->
+    <nav class="navbar navbar-expand-lg navbar-dark sticky-top">
+        <div class="container">
+            <a class="navbar-brand" href="#">
+                <i class="fas fa-landmark"></i>
+                MyMPP MBSA
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto align-items-center">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#home">Utama</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#ahli">Ahli Majlis</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#perkhidmatan">Perkhidmatan</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#proses">Proses</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#hubungi">Hubungi</a>
+                    </li>
+                    <li class="nav-item ms-3">
+                        <a href="#" class="btn btn-login-nav">
+                            <i class="fas fa-sign-in-alt me-2"></i>Log Masuk
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <!-- Navbar - End -->
+
+    <!-- Hero Slider - Start -->
+    <!-- Hero Slider - End -->
+
+    <!-- Statistic Section - Start -->
+    <!-- Statistic Section - End -->
+
+    <!-- Ahli Majlis Section - Start -->
+    <!-- Ahli Majlis Section - End -->
+
+    <!-- Feature Section - Start -->
+    <!-- Feature Section - End -->
+
+    <!-- Process Section - Start -->
+    <!-- Process Section - End -->
+
+    <!-- CTA  Section - Start -->
+    <!-- CTA  Section - End -->
+
+    <!-- Footer - Start -->
+    <!-- Footer - End -->
+
     <!-- Script CDN -->
     <!-- Bootstrap Script -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+
     <!-- SwiperJS Script -->
+    <script src="https://cdn.jsdelivr.net/npm/swiper@12/swiper-bundle.min.js"></script>
 </body>
 
 </html>
