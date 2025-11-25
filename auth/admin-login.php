7c6f7420d76f5e1e8aa06142ed0857c27a1af056
ahmadaimandev <aimanzulbahari@gmail.com>
2025-11-26 00:39:58 +0800

    add changes

---
+++ auth/admin-login.php
@@ -0,0 +1,19 @@
+<?php
+// Logic Goes Here
+?>
+
+<!-- Update New UI -->
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Log Masuk | Pentadbir Sistem</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
